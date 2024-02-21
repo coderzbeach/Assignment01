3892ff7b473c4becf58a4431dbf3d66df58e29ab
Alex Dolgoter <138642302+coderzbeach@users.noreply.github.com>
2024-02-20 23:20:37 -0800

    initial push to respository

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+let someMonth; //camel casing STEP 1
+function theMonth(); //camel casing STEP 1
+let currentMonth; //camel casing STEP 1
+let summerMonth; //camel casing STEP 1
+let myLibraryFunction; //camel casing STEP 1
+
+4.4 //numeric literal expression STEP 2
+'vase' //string literal STEP 2
+true // Boolean literal expression STEP 2
+null // null literal expression STEP 2
+
+let anExample = 5 / (6 * 3) - 3; // first example of complex or variable expression STEP 3
+let acExample = 5 + (10 /2); // second example of complex or variable expression STEP 3
+
+let strFirstName, strLastName, strAddress, strCity, strState, strZipCode, intYourAge, strReferralSource, booMayWeContactYou //declare 9 variables using Camel casing and Hungarian casing STEP 4
+
+//variable assignment method 1 STEP 5 
+//variable assignment method 2 STEP 5 
+//variable assignment method 3 STEP 5 
